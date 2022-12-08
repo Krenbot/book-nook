dad635e4d8f0880303397a127e6e20ee11a47fb3
OilPainter1 <hjcryns@gmail.com>
2022-12-07 19:51:07 -0600

    added login view route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const sequelize = require('./config/connection')
 const express = require('express')
-const routes = require('./controllers')
+const path = require("path")
 const { engine } = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -9,14 +9,19 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.json())
-app.use(routes)
 
-sequelize.sync({ force: false }).then(() => {
+app.get("/", async(req,res)=>{
+    res.render("home")
+})
+app.get("/login", (req,res)=>{
+    res.render("login")
+})
+
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => {
         console.log(`server listening @ http://localhost:${PORT}`)
     })
 })
 
-app.listen(PORT)
