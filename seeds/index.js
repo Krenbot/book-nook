4ee8606781c4f2649e1e5553bc1a8f8a3712aa6e
Steve Krenn <stevekrenn@gmail.com>
2022-12-07 20:39:13 -0600

    added seed data modulazation to index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,24 @@
 const sequelize = require('../config/connection');
 const { User, Review, Book, Comment } = require('../models');
 
-const userSeedData = require('./user-seeds');
-const bookSeedData = require('./book-seeds');
-const reviewSeedData = require('./review-seeds');
-const commentSeedData = require('./comment-seeds');
+const userData = require('./userSeedData.json');
+const bookData = require('./bookSeedData.json');
+const reviewData = require('./reviewSeedData.json');
+const commentData = require('./commentSeedData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await userData();
+    console.log('\n----- USERS SEEDED -----\n');
+    await bookData();
+    console.log('\n----- BOOKS SEEDED -----\n');
+    await reviewData();
+    console.log('\n----- REVIEWS SEEDED -----\n');
+    await commentData();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+
+    process.exit(0);
+}
+
+seedDatabase()
