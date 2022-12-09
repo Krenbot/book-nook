fc7ce980a4c5fce765b480ea1daa96e991c2a9fc
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-12-08 20:16:20 -0600

    adds home routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { Book, Comment, Review, User } = require('../models')
+const withAuth = require('..utils/auth');
+
+router.get('/book/:id', async (req, res) => {
+    try {
+      const bookData = await book.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const book = bookData.get({ plain: true });
+  
+      res.render('book', {
+        ...book,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.get('/home', withAuth, async (req, res) => {
+    try {
+      
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Project }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('home', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    
+    if (req.session.logged_in) {
+      res.redirect('/home');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  module.exports = router;
