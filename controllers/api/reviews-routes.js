b1a9108729f723bcd54eab16421e7d42dd5d95ea
Steve Krenn <stevekrenn@gmail.com>
2022-12-12 19:32:06 -0600

    added comment / review routes. added sample seed comments

---
+++ controllers/api/reviews-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { Review } = require('../../models/Review');
+
+//GET a review
+//GET all review
+//POST a review
+//UPDATE a review
+//DELETE a review
