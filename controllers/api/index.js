c1d1f4b267bb1acc419b71c156b987e5c7a73458
Steve Krenn <stevekrenn@gmail.com>
2022-12-14 18:17:29 -0600

    changed login variables, edited user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const commentRoutes = require('express').Router();
 const reviewRoutes = require('express').Router();
+const userRoutes = require('express').Router();
 
 router.use('/comments', commentRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
